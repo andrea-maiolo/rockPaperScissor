3b77e81931e89643d55433aa9fa59545334168d4
Andrea Maiolo <maiolo.m.2@gmail.com>
2020-04-17 11:53:26 +0200

    final game with buttons and resutls displayed! enjoy playing

--- main.js
+++ main.js
@@ -8,98 +8,107 @@ scissor.addEventListener('click', playerPlay);
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('click', start));
 
-const results = document.querySelector('.results');
+const gameover = document.querySelector('#gameover');
+const results = document.querySelector('#results');
 const para = document.createElement('p');
+const over = document.createElement('button');
+over.style.display = 'none';
+over.setAttribute('id', 'overButton')
 
+var playerSelection
+function playerPlay(e) {
+	playerSelection = this.id;
+}
 
 function computerPlay() {
-    function random() {
-        return Math.floor(Math.random() * 3)
-    }
-    if (random() == 0) {
-        return "rock"
-    }
-    else if (random() == 1) {
-        return "paper"
-    }
-    else {
-        return "scissor"
-    }
-}
-//var in wich playerSelection is stored    
-var a
-
-function playerPlay(e) {
-    a = this.id;
+	function random() {
+		return Math.floor(Math.random() * 3)
+	}
+	if (random() == 0) {
+		return "rock"
+	} else if (random() == 1) {
+		return "paper"
+	} else {
+		return "scissor"
+	}
 }
 
 function game1(p, c) {
-    //this check for P>R
-    if (p === "paper" && c === "rock") {
-        checkA++
-        return "You won! Paper beats rock"
-        //this check for P<S
-    }
-    else if (p === "paper" && c === "scissor") {
-        checkB++
-        return "You lose! Scissor beats paper"
-        //this check for R>S
-    }
-    else if (p === "rock" && c === "scissor") {
-        checkA++
-        return "You won! Rock beats scissor"
-        //this check for R<P
-    }
-    else if (p === "rock" && c === "paper") {
-        checkB++
-        return "You lose! Paper beats rock"
-        //this check for S<R
-    }
-    else if (p === "scissor" && c === "rock") {
-        checkB++
-        return "You lose! Rock beats scissor"
-        //this check for S>P
-    }
-    else if (p === "scissor" && c === "paper") {
-        checkA++
-        return "You won! Scissor beats paper"
-        //this check for even
-    }
-    else if (p === c) {
-        return "That is even"
-    }
+	if (p === "paper" && c === "rock") {
+		userScore++
+		return "You won! Paper beats rock"
+	} else if (p === "paper" && c === "scissor") {
+		computerScore++
+		return "You lose! Scissor beats paper"
+	} else if (p === "rock" && c === "scissor") {
+		userScore++
+		return "You won! Rock beats scissor"
+	} else if (p === "rock" && c === "paper") {
+		computerScore++
+		return "You lose! Paper beats rock"
+	} else if (p === "scissor" && c === "rock") {
+		computerScore++
+		return "You lose! Rock beats scissor"
+	} else if (p === "scissor" && c === "paper") {
+		userScore++
+		return "You won! Scissor beats paper"
+	} else if (p === c) {
+		return "That is even"
+	}
 }
 
-function start(event) {
-    // console.log(game1(a,computerPlay()))
-    para.textContent = (game1(a, computerPlay()))
-    results.appendChild(para);
+let userScore = 0;
+const player = document.querySelector('#player');
+player.textContent = 'Player: ' + userScore;
+let computerScore = 0;
+const computer = document.querySelector('#computer');
+computer.textContent = 'Computer: ' + computerScore;
+
+function checker(a, b) {
+	if (a == 5) {
+		over.innerHTML = 'You won, congratulations!';
+		over.style.display = 'block';
+		gameover.appendChild(over);
+		return true
+	} else if (b == 5) {
+		over.innerHTML = 'The computer won, try one more time';
+		over.style.display = 'block';
+		gameover.appendChild(over);
+		return true
+	} else {
+		return false
+	}
 }
 
+function reset() {
+	userScore = 0;
+	player.textContent = 'Player: ' + userScore;
+	computerScore = 0;
+	computer.textContent = 'Player: ' + userScore;
+	results.removeChild(para);
+	gameover.removeChild(over);
+	over.style.display = 'none';
+	rock.addEventListener('click', playerPlay);
+	paper.addEventListener('click', playerPlay);
+	scissor.addEventListener('click', playerPlay);
+	buttons.forEach(button => button.addEventListener('click', start));
+}
 
-//this variable check the victories of user
-let checkA = 0;
-//this variable check the victories of computer
-let checkB = 0;
-
-
-// //start with i=1 so it can log out game1,which sounds better than game0
-// for (let i=1; i<6; i++){
-
-//         console.log("Game " + i +" "+ game1(playerPlay(),computerPlay()));
-// }
-
-
-// function checker(checkB , checkA){
-// if (checkA > checkB){
-//         console.log("You won, congratulations!")
-//     }else if(checkB > checkA){
-//         console.log("The computer won, try one more time")
-//     }else if(checkA === undefined || checkB === undefined || checkA === null || checkB === null){
-//         console.log("Huston, we have a problem")
-//     }else{
-//         console.log("It's a tie!")
-//     }
-// }
-
-// checker(checkB, checkA);
+function start() {
+	let begin = game1(playerSelection, computerPlay());
+	para.textContent = begin;
+	results.appendChild(para);
+	gameover.appendChild(over);
+	computer.textContent = 'Computer: ' + computerScore;
+	player.textContent = 'Player: ' + userScore;
+	if (checker(userScore, computerScore) == true) {
+		buttons.forEach(button => button.removeEventListener('click', start));
+		rock.removeEventListener('click', playerPlay);
+		paper.removeEventListener('click', playerPlay);
+		scissor.removeEventListener('click', playerPlay);
+		over.addEventListener('click', reset);
+	} else {
+		//do something with this??
+		console.log('keep playing')
+	}
+}
